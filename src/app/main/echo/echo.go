1b2dd742f44e7b0a40b7b9dbf66ca12514616b6e
shellus <shellus@endaosi.com>
2017-04-21 08:03:47 +0800

    成功发送http请求

---
+++ src/app/main/echo/echo.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"bufio"
+	"strconv")
+
+const PORT = 3540
+
+func main() {
+	server, err := net.Listen("tcp", ":" + strconv.Itoa(PORT))
+	if server == nil {
+		panic(err)
+	}
+	conns := clientConns(server)
+	for {
+		go handleConn(<-conns)
+	}
+}
+
+func clientConns(listener net.Listener) chan net.Conn {
+	ch := make(chan net.Conn)
+	i := 0
+	go func() {
+		for {
+			client, err := listener.Accept()
+			if client == nil {
+				fmt.Errorf("couldn't accept: %s", err.Error())
+				continue
+			}
+			i++
+			fmt.Printf("%d: %v <-> %v\n", i, client.LocalAddr(), client.RemoteAddr())
+			ch <- client
+		}
+	}()
+	return ch
+}
+
+func handleConn(client net.Conn) {
+	b := bufio.NewReader(client)
+	for {
+		line, err := b.ReadBytes('\n')
+		if err != nil {
+			// EOF, or worse
+			break
+		}
+		client.Write(line)
+	}
+}
--- src/app/main/socket/client.go
+++ src/app/main/socket/client.go
@@ -17,21 +17,35 @@ func main() {
 
 	eol := "\r\n"
 
+	headers := []string{
+		"Host: 127.0.0.1",
+		"Connection: keep-alive",
+		"Cache-Control: max-age=0",
+		"Upgrade-Insecure-Requests: 1",
+		"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+		//"Accept-Encoding: gzip, deflate, sdch, br",
+		"Accept-Language: zh-CN,zh;q=0.8",
+	}
+
 	tcpAddr, _ := net.ResolveTCPAddr("tcp4", addr)
 
 	conn, _ := net.DialTCP("tcp", nil, tcpAddr)
 
-
-
 	buf := bufio.NewWriter(conn);
 
-	buf.WriteString(method + path + proto + eol)
+	buf.WriteString(method + " " + path + " " + proto + eol)
 
+	for i := 0; i < len(headers); i++ {
+		buf.WriteString(headers[i] + eol)
+	}
 	buf.WriteString(eol)
+
+	buf.Flush()
 
 	buf_r := make([]byte, 2014)
 
-	conn.Read(buf_r)
+	recv_len, _ := conn.Read(buf_r)
 
-	fmt.Print("over")
+	fmt.Printf("over %d : %s", recv_len, buf_r)
 }
