e895309a484c9a560f6ac90e2aaa1df67c0e7291
shellus <shellus@endaosi.com>
2017-05-01 14:06:48 +0800

    邋遢写法

--- src/crawl/mingxing/main.go
+++ src/crawl/mingxing/main.go
@@ -8,46 +8,185 @@ import (
 	"os"
 	"io"
 	"path/filepath"
+	"os/user"
 )
 
+// 明星详情页结构
 type MingXingItem struct {
 	name string
 	url  string
 }
+// 相册结构
 type XiangCeItem struct {
-	name string
-	url  string
+	mingXingItem MingXingItem
+	name         string
+	url          string
+}
+// 图片结构
+type TuPianItem struct {
+	xiangCeItem XiangCeItem
+	url         string
 }
 
-const storePath = `C:\Users\shellus\Pictures\明星图片`
+var userPath string
+var storePath = `/Pictures/明星图片`
+var baseUrl = "https://www.houyuantuan.com"
 
 func main() {
-	baseUrl := "https://www.houyuantuan.com"
-	c := cMingXingList(baseUrl + "/mingxing/2/")
-	for _, mingXingItem := range c {
 
-		fmt.Println(baseUrl + mingXingItem.url)
-		xiangces := cMingXing(baseUrl + mingXingItem.url)
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	userPath = usr.HomeDir
+	storePath = userPath + storePath
 
-		for _, xiangCeItem := range xiangces {
+	exit := make(chan bool)
 
-			fmt.Println(baseUrl + xiangCeItem.url)
-			u2 := cXiangCe(baseUrl + xiangCeItem.url)
+	b := boot()
 
-			for _, u3 := range u2 {
+	o1 := make(chan MingXingItem, 1)
 
-				fn := storePath + "\\" + mingXingItem.name + "\\" + xiangCeItem.name + "\\" + path.Base(u3)
-				fmt.Println("http:" + u3)
-				fmt.Println(fn)
+	f1(b, o1)
+	f1(b, o1)
+	f1(b, o1)
+	f1(b, o1)
+	f1(b, o1)
+	f1(b, o1)
+	f1(b, o1)
+	f1(b, o1)
 
-				res, _ := http.Get("http:" + u3)
+	o2 := make(chan XiangCeItem, 1)
 
-				os.MkdirAll(filepath.Dir(fn), os.FileMode(777))
-				file, _ := os.Create(fn)
-				io.Copy(file, res.Body)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+	f2(o1, o2)
+
+	o3 := make(chan TuPianItem, 1)
+
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+	f3(o2, o3)
+
+	dc := 15
+
+	for i := 0; i < dc; i++ {
+		downloader(o3, exit)
+	}
+
+	for i := 0; i < dc; i++ {
+		<-exit
+	}
+
+}
+func boot() chan string {
+	o := make(chan string, 1)
+	go func() {
+		o <- baseUrl + "/mingxing/2/"
+		close(o)
+	}()
+	return o
+}
+
+// 列表工厂
+// 输入列表url
+// 输出详情
+func f1(i chan string, o chan MingXingItem) {
+
+	go func() {
+		for u := range i {
+			for _, item := range cMingXingList(u) {
+				o <- item
 			}
 		}
-	}
+	}()
+}
+
+// 详情工厂
+// 输入详情
+// 输出相册
+func f2(i chan MingXingItem, o chan XiangCeItem) {
+
+	go func() {
+		for u := range i {
+			for _, item := range cMingXing(u) {
+				o <- item
+			}
+		}
+	}()
+}
+
+// 相册工厂
+// 输入相册
+// 输出图片
+func f3(i chan XiangCeItem, o chan TuPianItem) {
+
+	go func() {
+		for u := range i {
+			for _, item := range cXiangCe(u) {
+				o <- item
+			}
+		}
+	}()
+}
+
+func downloader(i chan TuPianItem, e chan bool) {
+	go func() {
+		for u := range i {
+			fn := storePath + "\\" + u.xiangCeItem.mingXingItem.name + "\\" + u.xiangCeItem.name + "\\" + path.Base(u.url)
+			fmt.Println("http:" + u.url)
+			fmt.Println(fn)
+
+			res, _ := http.Get("http:" + u.url)
+
+			os.MkdirAll(filepath.Dir(fn), os.FileMode(777))
+			file, _ := os.Create(fn)
+			io.Copy(file, res.Body)
+		}
+		e <- true
+	}()
 }
 
 // 采集明星列表，返回详情页url列表
@@ -55,11 +194,8 @@ func cMingXingList(url string) (l []MingXingItem) {
 
 	doc, _ := goquery.NewDocument(url)
 	_ = doc.Find("body > div.wrapper > div.container > div > div.mod-list > div.hot > ul > li").Map(func(i int, s *goquery.Selection) string {
-		// :nth-child(1) > a.avatar > img
-		// a.name
 		name := s.Find("a.name").Text()
 		href, _ := s.Find("a.name").Attr("href")
-		//avatar_img_src, _ := s.Find("a.avatar > img").Attr("src")
 		l = append(l, MingXingItem{
 			name: name,
 			url: href,
@@ -69,17 +205,18 @@ func cMingXingList(url string) (l []MingXingItem) {
 	return
 }
 
-// 采集明星详情页面, 返回相册url列表
-func cMingXing(url string) (l []XiangCeItem) {
-	doc, _ := goquery.NewDocument(url)
 
-	// body > div.wrapper > div.container > div > div.mod-main > div.modules.pic > ul > li:nth-child(1) > div.cover > a > img
+// 采集明星详情页面, 返回相册url列表
+func cMingXing(mingXingItem MingXingItem) (l []XiangCeItem) {
+
+	doc, _ := goquery.NewDocument(baseUrl + mingXingItem.url)
+
 	_ = doc.Find("body > div.wrapper > div.container > div > div.mod-main > div.modules.pic > ul > li").Map(func(i int, s *goquery.Selection) string {
-		// 相册url
 		href, _ := s.Find("div.cover > a").Attr("href")
 
 		name := s.Find("div.cover-title > p > a").Text()
 		l = append(l, XiangCeItem{
+			mingXingItem: mingXingItem,
 			name: name,
 			url: href,
 		})
@@ -92,15 +229,17 @@ func cMingXing(url string) (l []XiangCeItem) {
 
 
 // 获取相册图片url列表
-func cXiangCe(url string) []string {
-	doc, _ := goquery.NewDocument(url)
+func cXiangCe(xiangCeItem XiangCeItem) (l []TuPianItem) {
+	doc, _ := goquery.NewDocument(baseUrl + xiangCeItem.url)
 
-	// body > div.wrapper > div.container > div > div.mod-atlas > div.bd > div > div > ul:nth-child(1) > li:nth-child(1) > div.pic > img
-	urls := doc.Find("body > div.wrapper > div.container > div > div.mod-atlas > div.bd > div > div > ul:nth-child(1) > li").Map(func(i int, s *goquery.Selection) string {
-		// 相册url
+	doc.Find("body > div.wrapper > div.container > div > div.mod-atlas > div.bd > div > div > ul:nth-child(1) > li").Map(func(i int, s *goquery.Selection) string {
 		href, _ := s.Find("div.pic > img").Attr("src")
-		return href
+		l = append(l, TuPianItem{
+			xiangCeItem: xiangCeItem,
+			url: href,
+		})
+		return ""
 	})
 
-	return urls;
+	return
 }
