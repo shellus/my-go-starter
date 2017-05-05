1cd79158045588bf1e7fea96e678af038ed9634b
shellus <shellus@endaosi.com>
2017-05-05 21:05:37 +0800

    ajax demo

--- src/http/server/main.go
+++ src/http/server/main.go
@@ -2,14 +2,45 @@ package main
 
 import (
 	"net/http"
-	"fmt"
+	"encoding/json"
 )
 
-func main() {
-	http.Handle("/asset", http.FileServer(http.Dir(`C:\data\c\frp`)))
+// api 接口返回的通用格式
+type ApiResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Data    interface{} `json:"data"`
+}
 
-	http.Handle("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(w, "哈哈哈")
+// 登录接口返回的用户基本资料
+type UserData struct {
+	Name string `json:"name"`
+	Id   int `json:"id"`
+}
+
+func main() {
+	http.Handle("/asset/", http.StripPrefix("/asset/", http.FileServer(http.Dir(`asset`))))
+
+	http.HandleFunc("/api/login", func(w http.ResponseWriter, req *http.Request) {
+
+		// 定义返回值
+		r := ApiResponse{
+			"ok",
+			"登录成功",
+			UserData{
+				"shellus",
+				1,
+			},
+		}
+
+		// 序列化成json
+		j, e := json.Marshal(r)
+		if e != nil {
+			panic(e)
+		}
+
+		// 发送数据
+		w.Write(j)
 	})
 
 	http.ListenAndServe(":8080", nil)
