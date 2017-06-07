cb0a85d9c8ebda1e69ed1ca9b865d4ab8030891f
shellus <shellus@endaosi.com>
2017-06-07 14:54:50 +0800

    queue改成基于redis

--- src/queue/queue.go
+++ src/queue/queue.go
@@ -1,53 +1,63 @@
 package queue
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+	"encoding/json"
+	"time"
+)
 
+const prefix string = "queue"
 // go的任务队列，或者说goroutine管理器
 // 创建一个queue(NewQueue)并设置并发数，绑定一个消费者(Sub)，然后Push一堆任务。然后Work阻塞执行。执行完了之后，就会退出Work方法。
 
 type queue struct {
-	jobs       chan Job
-	concurrent chan bool
-	subscriber func(j Job)(err error)
+	channelName string
+	concurrent  chan bool
+	subscriber  func(j Job) (err error)
+	redis       *redis.Client
 }
 
 type Job struct {
 	Value interface{}
 }
 
-func NewQueue(concurrentNumber int, channel string) (q *queue) {
+func NewQueue(concurrentNumber int, channelName string) (q *queue) {
 	// channel is back
 	q = &queue{
+		channelName: channelName,
 		concurrent: make(chan bool, concurrentNumber),
-		jobs: make(chan Job, 10000),
+		redis: redis.NewClient(&redis.Options{
+			Addr:     "127.0.0.1:6379",
+			Password: "", // no password set
+			DB:       0, // use default DB
+		}),
 	}
 	return
 }
 
-func (q *queue) Push(j Job) {
-	q.jobs <- j
+func (q *queue) Push(j *Job) {
+	q.redis.LPush(prefix + ":" + q.channelName, serialization(j))
 }
 
-func (q *queue) Sub(f func(j Job)(err error)) {
+func (q *queue) Sub(f func(j Job) (err error)) {
 	q.subscriber = f
 }
 func (q *queue) Work() {
-	//L:
 	for {
-		select {
-		case j := <-q.jobs:
-			q.concurrent <- true
-			go q.call(j)
-		//default:
-		//	for i := 0; i < cap(q.concurrent); i++ {
-		//		q.concurrent <- true
-		//	}
-			//break L
+		s, err := q.redis.BRPopLPush(prefix + ":list:" + q.channelName, prefix + ":run:" + q.channelName, time.Minute).Result()
+		if err != nil {
+
 		}
+
+		j := &Job{}
+		deserialization(s[1], j)
+		q.concurrent <- true
+		go q.call(j)
 	}
 
 }
-func (q *queue) call(j Job) {
+func (q *queue) call(j *Job) {
 	defer func() {
 		<-q.concurrent
 		// 还是不要拦截这些致命错误了
@@ -59,11 +69,28 @@ func (q *queue) call(j Job) {
 	}()
 
 	// call
-	err := q.subscriber(j)
+	err := q.subscriber(*j)
 
 	// 如果出错，把job放回去
 	if err != nil {
 		q.Push(j)
 		fmt.Printf("%+v\n", err)
 	}
+
+}
+
+func serialization(j *Job) (s string) {
+	buf, err := json.Marshal(j)
+	if err != nil {
+		return ""
+	}
+	return string(buf)
+}
+
+func deserialization(s string, j *Job) (err error) {
+	err = json.Unmarshal([]byte(s), j)
+	if err != nil {
+		return err
+	}
+	return nil
 }
